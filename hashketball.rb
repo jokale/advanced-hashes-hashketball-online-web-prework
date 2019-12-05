7dd06689bb6a6ae7b2db0631842996a0344581fa
Joanna E K <kalemajoanna@gmail.com>
2019-12-05 18:59:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,6 +117,172 @@ def game_hash
   
   return hashgame 
 end
+def num_points_scored(arg)
+    game_hash.each do |key, value| 
+       value.each do |other_key, other_value|
+          if other_key == :players 
+            other_value.each do |name|
+              if name[:player_name] == arg
+                return name[:points] 
+              end   
+            end   
+          end   
+       end  
+    end  
+end  
 
+def shoe_size(arg)
+
+    game_hash.each do |key, value| 
+       value.each do |other_key, other_value|
+          if other_key == :players 
+            other_value.each do |shoe|
+              if shoe[:player_name] == arg
+                return shoe[:shoe] 
+              end   
+            end   
+          end   
+       end  
+    end  
+end 
+
+def team_colors(arg)
+     game_hash.each do |key, value| 
+      value.each do |other_key, other_value|
+        if other_key == :team_name
+            if other_value == arg
+              return value[:colors]
+            end 
+        end 
+      end  
+    end 
+end
+
+def team_names 
+arr = []
+  game_hash.each do |key, value|
+    arr << value[:team_name]
+  end
+  return arr
+end  
+
+def player_numbers(arg)
+  game_hash.each do |key, value|
+    if value[:team_name] == arg
+      return value[:players].map {|x| x[:number]}
+    end
+  end
+end 
+
+def player_stats(arg)
+hashy_stats = {}
+  game_hash.each do |key, value|
+    value[:players].each do |one|
+      if one[:player_name] == arg
+        hashy_stats = one
+      end
+    end
+  end
+  hashy_stats.delete(:player_name)
+  return hashy_stats
+end 
+
+
+def big_shoe_rebounds
+
+big_shoe = 0
+rebounds = 0
+
+game_hash.each do |key, value|
+ value[:players].each do |name|
+  if name[:shoe] > big_shoe
+    big_shoe = name[:shoe]
+    rebounds = name[:rebounds]
+  end  
+ end
+end
+return rebounds
+
+end  
+
+def most_points_scored
+  points = 0
+name = ''
+game_hash.each do |key, value|
+  value[:players].each do |vals|
+  if vals[:points] > points
+    points = vals[:points]
+    name = vals[:player_name]
+  end
+  end 
+end
+return name
+end  
+
+def winning_team
+  home_points = 0
+away_points = 0
+
+game_hash.each do |key, value|
+  if key == :home
+  value[:players].each do |name|
+    home_points += name[:points]
+  end
+  else 
+  value[:players].each do |name|
+    away_points += name[:points]
+  end
+  end
+end
+
+if home_points > away_points
+  return "Brooklyn Nets"
+else
+  return "Charlotte Hornets"  
+end
+
+end  
+
+def player_with_longest_name
+name_arr = []
+game_hash.each do |key, value|
+  value[:players].each do |name|
+    name_arr << name[:player_name]
+  end
+end
+
+sorted_arr = name_arr.sort_by {|x| x.length}
+
+return sorted_arr[-1]
+end  
+
+
+def big_steal 
+  biggest_steal = 0
+game_hash.each do |key, value|
+  value[:players].each do |name|
+      if name[:steals] > biggest_steal
+        biggest_steal = name[:steals]
+      end
+  end
+end  
+return biggest_steal
+end   
+
+
+
+
+
+def long_name_steals_a_ton?
+big = big_steal
+long = player_with_longest_name 
+
+if big == long 
+  return FALSE
+else 
+  return TRUE
+end   
+
+end 
  
 
